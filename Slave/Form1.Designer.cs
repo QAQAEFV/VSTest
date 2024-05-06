1df9f3abc7920df04b594923d424e83e96e89406
jcq <2232752402@qq.com>
2024-05-06 15:04:40 +0800

    第一次提交

---
+++ Slave/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Slave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Creat = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.IpAddressTextBox = new System.Windows.Forms.TextBox();
+            this.IpAddress = new System.Windows.Forms.Label();
+            this.PortNum = new System.Windows.Forms.Label();
+            this.PortNumTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Creat
+            // 
+            this.Creat.Location = new System.Drawing.Point(518, 57);
+            this.Creat.Name = "Creat";
+            this.Creat.Size = new System.Drawing.Size(75, 23);
+            this.Creat.TabIndex = 0;
+            this.Creat.Text = "Creat";
+            this.Creat.UseVisualStyleBackColor = true;
+            this.Creat.Click += new System.EventHandler(this.Creat_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(53, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(670, 319);
+            this.listBox1.TabIndex = 1;
+            // 
+            // IpAddressTextBox
+            // 
+            this.IpAddressTextBox.Location = new System.Drawing.Point(110, 55);
+            this.IpAddressTextBox.Name = "IpAddressTextBox";
+            this.IpAddressTextBox.Size = new System.Drawing.Size(156, 25);
+            this.IpAddressTextBox.TabIndex = 2;
+            this.IpAddressTextBox.Text = "127.0.0.1";
+            // 
+            // IpAddress
+            // 
+            this.IpAddress.AutoSize = true;
+            this.IpAddress.Location = new System.Drawing.Point(40, 58);
+            this.IpAddress.Name = "IpAddress";
+            this.IpAddress.Size = new System.Drawing.Size(68, 15);
+            this.IpAddress.TabIndex = 3;
+            this.IpAddress.Text = "Ip地址：";
+            // 
+            // PortNum
+            // 
+            this.PortNum.AutoSize = true;
+            this.PortNum.Location = new System.Drawing.Point(297, 58);
+            this.PortNum.Name = "PortNum";
+            this.PortNum.Size = new System.Drawing.Size(52, 15);
+            this.PortNum.TabIndex = 5;
+            this.PortNum.Text = "端口：";
+            // 
+            // PortNumTextBox
+            // 
+            this.PortNumTextBox.Location = new System.Drawing.Point(355, 55);
+            this.PortNumTextBox.Name = "PortNumTextBox";
+            this.PortNumTextBox.Size = new System.Drawing.Size(100, 25);
+            this.PortNumTextBox.TabIndex = 4;
+            this.PortNumTextBox.Text = "502";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PortNum);
+            this.Controls.Add(this.PortNumTextBox);
+            this.Controls.Add(this.IpAddress);
+            this.Controls.Add(this.IpAddressTextBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Creat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Creat;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox IpAddressTextBox;
+        private System.Windows.Forms.Label IpAddress;
+        private System.Windows.Forms.Label PortNum;
+        private System.Windows.Forms.TextBox PortNumTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Slave/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Modbus.Device;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modbus.Data;
+
+namespace Slave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 服务器的创建
+        bool isServerCreated;
+        bool creatServer;
+        int count2;
+        TcpListener listener;
+        ModbusSlave slave;
+        DataStore store;
+
+        private void Creat_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), Convert.ToInt32("502"));
+                listener.Start();
+                slave = ModbusTcpSlave.CreateTcp(2, listener);//创建对象
+                store = DataStoreFactory.CreateDefaultDataStore();
+                slave.DataStore = store;
+/*                slave.DataStore = Modbus.Data.DataStoreFactory.CreateDefaultDataStore();//用于清除Modbus TCP Slave的数据存储区，寄存器区值全部写0*/
+                listBox1.Items.Add("创建服务器成功！" + "线程ID：" + Thread.CurrentThread.ManagedThreadId + "\r\n");
+                //订阅接收到报文请求事件，可以打印接收到的报文
+                slave.ModbusSlaveRequestReceived += ModbusSlaveRequestReceivedHandle;
+                //订阅接收到写入完成事件，可以打印写入完成响应
+                slave.WriteComplete += WriteCompleteHandle;
+                slave.Listen();
+                isServerCreated = true;
+                listBox1.Items.Add("服务器创建成功！" + "\r\n");
+                count2 = 0;
+            }
+            catch (Exception ex)
+            {
+                count2++;
+                listBox1.Items.Add("创建服务器失败！" + "失败次数:" + count2.ToString() + "\r\n");
+                isServerCreated = false;
+                creatServer = false;
+                return;
+            }
+        }
+
+        private void ModbusSlaveRequestReceivedHandle(object obj, ModbusSlaveRequestEventArgs e)
+        {
+            string str = "";
+            foreach (var item in e.Message.MessageFrame)
+            {
+                str += item.ToString("x2").PadLeft(2, '0').ToUpper() + "  ";
+            }
+            listBox1.Items.Add("服务器收到报文:  " + str + "\r\n");
+        }
+        private void WriteCompleteHandle(object sender, ModbusSlaveRequestEventArgs e)
+        {
+            string str = "";
+            foreach (var item in e.Message.MessageFrame)
+            {
+                str += item.ToString("x2").PadLeft(2, '0').ToUpper() + "  ";
+            }
+            listBox1.Items.Add("服务器写入完成:  " + str + "\r\n");
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ushort address = 1000;
+            ushort[] arry = new ushort[100];
+            for(int i = 0; i < 100; i++)
+            {
+                arry[i] = store.HoldingRegisters[i];
+            }
+            string temp = "";
+            for(int i = 0;i<arry.Length;i++)
+            {
+                temp += arry[i].ToString()+"....";
+            }
+            MessageBox.Show(temp);
+        }
+    }
+}
